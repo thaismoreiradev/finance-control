c74af5e75b77860dc306de417296601b78bc1f2a
thaismoreiradev <contatothaismoreira@hotmail.com>
2023-02-03 21:02:31 -0300

    largest screen adjust

--- src/components/ResumeCard.jsx
+++ src/components/ResumeCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const ResumeCard = ({ title, value }) => {
   return (
-    <div className='bg-slate-700 text-white font-semibold w-[28vw] md:w-[25vw] xl:w-[23vw] flex flex-col items-center justify-center py-2 px-4 h-full w-screen md:max-w-[75vw] rounded-md'>
+    <div className='bg-slate-700 text-white font-semibold w-[28vw] md:w-[25vw] xl:w-[23vw] flex flex-col items-center justify-center py-2 px-4 h-full w-screen md:max-w-[75vw] lg:max-w-[18vw] 2xl:max-w-[15vw] rounded-md'>
       <h2 className='font-Titillium text-yellow-200 '>{title}</h2>
       <h3>{value}</h3>
     </div>
